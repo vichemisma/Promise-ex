51d9cd77607af92c67389b2f51c05c4eb04a8c63
Zafer Aycicek <vichemisma@gmail.com>
2024-10-06 20:45:55 +0100

    v5

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-let check = true;
+// let check = true;
+// //true ise console da Promise basarili
+// //false ise console da Promise basarisiz
 
-const promise1 = new Promise((resolve, reject) => {
-  if (check){
-    resolve("Promise basarili");
-  }else {
-    reject("Promise basarisiz...");
-  }
-});
+// const promise1 = new Promise((resolve, reject) => {
+//   if (check){
+//     resolve("Promise basarili");
+//   }else {
+//     reject("Promise basarisiz...");
+//   }
+// });
   
 
-console.log(promise1)
+// console.log(promise1)
+// let check = false;
+// function createPromise() {
+//   return new Promise ((resolve, reject) => {
+//     if (check) {
+//       resolve("Promiste herhangi bir sikinti yok...");
+//     }else {
+//       reject("Sikinti buyuk...");
+//     }
+//   })
+// }
+
+// createPromise()
+// .then((response) => { //resolve ile donerse then yakalr
+//   console.log(response);
+// })
+// .catch((error) => {//reject ile donerse catch yakalr
+//   console.log(error);
+  
+// })
+// .finally(() => {//finally her zaman yakalr
+//   console.log("her zaman");
+// })
+// //asenkron tapilari senkrona cevirir
+// // callback lerin alternatifi
+// // es6 ile yazildi
+
+// ? PROMISE + XMLHTTPREQUEST
