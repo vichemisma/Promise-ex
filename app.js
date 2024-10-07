976661952877af052dd8b3fd08586c8ef8784167
Zafer Aycicek <vichemisma@gmail.com>
2024-10-07 14:03:54 +0100

    v7

--- app.js
+++ app.js
@@ -80,7 +80,32 @@ function getUsers(url){
   })
 }
 
-getUsers("https://jsonplaceholder.typicode.com/users")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err))
-  .finally(() => console.log("finally"))
+function getCommentsByUserId(url){
+  return new Promise((resolve,reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange",() => {
+      try {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));}
+      } catch (error) {
+        reject(error)
+        
+      }
+    })
+
+    xhr.open("GET",url);
+    xhr.send();
+  })
+
+}
+getUsers("https://jsonplaceholder.typicode.com/users/3")
+.then((data) => {
+  console.log(data);
+  return getCommentsByUserId(`https://jsonplaceholder.typicode.com/comments/${data.id}`)
+})
+.then((res) => {
+  console.log(res);
+})
+.catch((err) => {
+  console.log(err);
+})
